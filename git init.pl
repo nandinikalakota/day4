f4ce9c5af61fe53cce83e932272dacb6aab8bd73
Nandini <kalakotanandini@gmail.com>
2023-08-07 14:16:00 +1000

    first commit

---
+++ git init.pl
@@ -0,0 +1,8 @@
+git init
+git add.
+git commit -m "first commit"
+git push -u origin main
+copy 3 lines of codes and paste it
+to update in the existing project:
+git add .
+git commit -m "explain what changes"
